97caf4070f8a612026049d1780e7a91076ecfa68
samuel <sam.ashagre25gmail.com>
2020-01-28 16:00:36 -0800

    Assignment 4

---
+++ replication/src/replication/replica.java
@@ -0,0 +1,26 @@
+
+package replication;
+
+public class replica {
+
+// TODO Auto-generated method stub
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		repl("Samuel", 3);
+		repl("Ashagre",0);
+		repl("Belvue college", -1);
+		
+		
+	}
+
+	public static void repl(String name, int times) {
+		for (int i=1; i<=times; i++) {
+			System.out.print(name);
+			
+		}
+			
+
+	
+	
+    }
+}
